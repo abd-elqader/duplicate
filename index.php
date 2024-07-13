1cd9b68bf7a1580a416e3adcb908791fa468ab32
abdelqader <abdelqader.montaser@gmail.com>
2024-07-13 22:41:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// strings
+$stringArray = ["apple", "banana", "apple", "orange", "banana"];
+$uniqueStrings = array_unique($stringArray);
+print_r($uniqueStrings);
+
+// integers
+$integersArray = [1, 2, 3, 1, 2, 4];
+$uniqueIntegers = array_unique($integersArray);
+print_r($uniqueIntegers);
+
+?>
